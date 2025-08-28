a8254b0c8de1390ff6748c8e10d9bb46a9ee8f2d
Geri <g.hausammann@computech.ch>
2025-08-28 10:51:06 +0200

    asdf

--- app.config.ts
+++ app.config.ts
@@ -2,11 +2,4 @@ import { defineAppConfig } from "nuxt/app";
 
 export default defineAppConfig({
   title: 'Hello Nuxt',
-  icon: 'mdi:home',
-  theme: {
-    dark: true,
-    colors: {
-      primary: '#ff0000'
-    }
-  }
 })
